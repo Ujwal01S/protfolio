c3b016d6df0d53e3c748e2d7778d0eda9f839223
Ujwal Suwal <rev477r.7@gmail.com>
2026-02-26 19:32:04 +0545

    chore:added new experience

--- src/components/modules/experience-section.tsx
+++ src/components/modules/experience-section.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import WenSVG from "../svg/wen-svg";
 import {
   Accordion,
@@ -14,6 +15,40 @@ const ExperienceSection = () => {
       </header>
       <Accordion type='single' collapsible>
         <AccordionItem value='item-1'>
+          <div className='flex justify-between items-center'>
+            <AccordionTrigger className='flex gap-2 items-center hover:no-underline flex-1'>
+              <figure className='relative w-[50px] h-[50px] rounded-full'>
+                <Image
+                  src={"/bridge.webp"}
+                  // height={100}
+                  // width={50}
+                  fill
+                  alt='logo'
+                  className='rounded-full border border-slate-100 bg-white p-2 object-contain'
+                />
+              </figure>
+              <div className='text-left'>
+                <p className='font-semibold text-normal'>
+                  Associate Software Engineering Developer
+                </p>
+                <p className='text-xs font-normal'>
+                  BRIDGE INTERNATIONAL CONSULTANCY AND MIGRATION LIMITED
+                </p>
+              </div>
+            </AccordionTrigger>
+            <p className='text-xs md:text-sm text-muted-foreground pr-4'>
+              Sep 2023 - Aug 2024
+            </p>
+          </div>
+
+          <AccordionContent className='pl-[64px] md:pl-[74px] py-0'>
+            As an Associate Software Engineering Developer, I contributed to the
+            development and maintenance of web applications, focusing on
+            frontend technologies and collaborating with cross-functional teams
+            to deliver high-quality solutions tailored to client needs.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value='item-2'>
           <div className='flex justify-between items-center'>
             <AccordionTrigger className='flex gap-2 items-center hover:no-underline flex-1'>
               <WenSVG />
